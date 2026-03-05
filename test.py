13efd5e32e5beed6431a5de93e84559fccd0adf4
Jakko Yang <jakko@umich.edu>
2026-03-05 03:02:36 -0500

    parsed

--- test.py
+++ test.py
@@ -12,13 +12,22 @@ def get_status(base_url, clinician_id): # sends requests
         return response.json()
     except requests.exceptions.RequestException:
         return None
+    
+def parse_status(raw):
+    loc = raw['features'][0]['geometry']['coordinates']
+    zone = raw['features'][1]['geometry']['coordinates'][0]
+    print("loc: " + str(loc))
+    print("zone: " + str(zone))
+    return [loc, zone]
 
 if __name__ == "__main__":    
     print("Testing id 1):")
     raw_1 = get_status(BASE_URL, 1)
     print(raw_1)
+    parsed_1 = parse_status(raw_1)
     
     print()
     print("Testing id 7:")
     raw_7 = get_status(BASE_URL, 7)
     print(raw_7)
+    parsed_7 = parse_status(raw_7)
